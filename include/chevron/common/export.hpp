376381c6a7deae9b3b6e88a35a44a408e7b14269
Jamon Bailey <infinity.dev@outlook.com>
2026-01-15 18:34:55 -0500

    Update export.hpp
    Clang-Format run

--- include/chevron/common/export.hpp
+++ include/chevron/common/export.hpp
@@ -12,40 +12,40 @@
  */
 
 #if defined(_WIN32) || defined(__CYGWIN__)
-  /*!
-   * @brief (Building Dynamic Link Library) Chevron public
-   * API macro for inline functions/variables, templates,
-   * and header bound entities.
-   */
-  #define CHEVRON_HEADER_API
-  #ifdef chevron_EXPORTS
     /*!
      * @brief (Building Dynamic Link Library) Chevron public
-     * API macro for classes, functions, and extern variables.
+     * API macro for inline functions/variables, templates,
+     * and header bound entities.
      */
-    #define CHEVRON_API __declspec(dllexport)
-  #else
-    /*!
-     * @brief Chevron public API macro for classes, functions,
-     * and extern variables.
-     */
-    #define CHEVRON_API __declspec(dllimport)
-  #endif
-#else
-  #ifdef chevron_EXPORTS
-    /*!
-     * @brief (Building Shared Object) Chevron public API macro
-     * for inline functions/variables, templates, and header
-     * bound entities.
-     */
-    #define CHEVRON_HEADER_API __attribute__((visibility("default")))
-    /*!
-     * @brief (Building Shared Object) Chevron public API macro
-     * for classes, functions, and extern variables.
-     */
-    #define CHEVRON_API __attribute__((visibility("default")))
-  #else
-    #define CHEVRON_API
     #define CHEVRON_HEADER_API
-  #endif
+    #ifdef chevron_EXPORTS
+        /*!
+         * @brief (Building Dynamic Link Library) Chevron public
+         * API macro for classes, functions, and extern variables.
+         */
+        #define CHEVRON_API __declspec(dllexport)
+    #else
+        /*!
+         * @brief Chevron public API macro for classes, functions,
+         * and extern variables.
+         */
+        #define CHEVRON_API __declspec(dllimport)
+    #endif
+#else
+    #ifdef chevron_EXPORTS
+        /*!
+         * @brief (Building Shared Object) Chevron public API macro
+         * for inline functions/variables, templates, and header
+         * bound entities.
+         */
+        #define CHEVRON_HEADER_API __attribute__((visibility("default")))
+        /*!
+         * @brief (Building Shared Object) Chevron public API macro
+         * for classes, functions, and extern variables.
+         */
+        #define CHEVRON_API __attribute__((visibility("default")))
+    #else
+        #define CHEVRON_API
+        #define CHEVRON_HEADER_API
+    #endif
 #endif
