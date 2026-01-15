ef708b01fb44ba5c49f1a8bc080bf7ac02f041f4
Jamon Bailey <infinity.dev@outlook.com>
2026-01-15 18:46:00 -0500

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -1,11 +1,72 @@
 
 #include <iostream>
+#include"chevron/model/function/func_args.hpp"
+#include"chevron/model/function/func_pointer.hpp"
+
+template <typename... ArgsT>
+using FuncArgs = chevron::model::FuncArgs<ArgsT...>;
+
+template <typename ReturnT, typename... ArgsT>
+using FuncPtr = chevron::model::FuncPtr<ReturnT, ArgsT...>;
+
+bool demoMethod(int val1, int val2)
+{
+    return val1 == val2;
+}
+
+char demoMethod2(bool good)
+{
+    return 'G';
+}
+
+int demoMethod3()
+{
+    return 100;
+}
+
+struct tester {
+    tester(const int initVal) : value{ initVal } {}
+
+    int getVal() const { return value; }
+    void setValue(const int newVal) { value = newVal; }
+    bool isMatch(const int val1, const int val2) const { return val1 == val2; }
+
+    int value;
+};
 
 int main(int argc, char* argv[])
 {
     //\\//
     //
 
-    std::cout << "\nFunctioning properly." << std::endl;
+    FuncArgs cmdlArgs{ argc, argv };
+
+    tester ggg{ 12 };
+
+    FuncArgs args{ 10, 10 };
+    FuncPtr demoCall{ demoMethod };
+    FuncPtr callPtr{ &ggg, &tester::isMatch };
+    FuncPtr freeCall{ demoMethod2 };
+    FuncPtr<int> noArgCall;
+
+    if (freeCall)
+        std::cout << freeCall(false) << std::endl;
+
+    if (callPtr)
+        std::cout << callPtr(args) << std::endl;
+
+    if (!noArgCall.bind(demoMethod3))
+        return -1;
+
+    if (!callPtr.bind(&ggg, &tester::isMatch))
+        return -1;
+
+    noArgCall();
+    callPtr(25, 16);
+
+    // struct FunctionModel --> ReturnType & Arguments --> FuncPtr or Func
+
+    // Static callable type? (No heap alloc)
+
     return 0;
 }
