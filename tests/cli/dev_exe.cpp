58248d7dcd7752ec74d40ea1b3050f7b56a7f835
Jamon Bailey <infinity.dev@outlook.com>
2026-01-17 14:25:17 -0500

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -1,76 +1,22 @@
 
 #include <iostream>
 #include <string>
-#include"chevron/model/function/func_args.hpp"
-#include"chevron/model/function/func_pointer.hpp"
 #include"chevron/common/export.h"
+#include"chevron/function.hpp"
 
-bool demoMethod(int val1, int val2)
+int doNothing()
 {
-    return val1 == val2;
+    return 15;
 }
-
-char demoMethod2(bool good)
-{
-    return 'G';
-}
-
-int demoMethod3()
-{
-    return 100;
-}
-
-struct tester {
-    tester(const int initVal) : value{ initVal } {}
-
-    int getVal() const { return value; }
-    void setValue(const int newVal) { value = newVal; }
-    bool isMatch(const int val1, const int val2) const { return val1 == val2; }
-
-    int operator()() const { return value; }
-
-    int value;
-};
 
 int main(int argc, char* argv[])
 {
     //\\//
     //
 
-    chevron::FuncArgs cmdlArgs{ argc, argv };
-
-    auto lambda = [&cmdlArgs](const int val, const char& te) -> bool {
-        return val > 75;
-    };
-
-    tester ggg{ 12 };
-
-    chevron::FuncArgs args{ 10, 10 };
-    chevron::FuncPtr demoCall{ demoMethod };
-    chevron::FuncPtr callPtr{ &ggg, &tester::isMatch };
-    chevron::FuncPtr nCallPtr{ &ggg, &tester::setValue };
-    chevron::FuncPtr freeCall{ demoMethod2 };
-    chevron::FuncPtr lambCall{ lambda };
-    chevron::FuncPtr functCall{ ggg };
-    chevron::FuncPtr<int> noArgCall;
-
-    if (freeCall)
-        std::cout << freeCall(false) << std::endl;
-
-    if (callPtr)
-        std::cout << callPtr(args) << std::endl;
-
-    if (!noArgCall.bind(demoMethod3))
-        return -1;
-
-    if (!callPtr.bind(&ggg, &tester::isMatch))
-        return -1;
-
-    noArgCall();
-    callPtr(25, 16);
-    callPtr(args);
-    lambCall(12, 'C');
-    functCall();
+    // Start...
+    chevron::IntReturnNoArgs ggg{doNothing};
+    chevron::NoArgsReturn<int> ddd{doNothing};
 
     //
     //\\//
@@ -78,8 +24,8 @@ int main(int argc, char* argv[])
     return 0;
 
     // FIX: Need specific constructor for lambda/functor using `::operator()`!!!
+    // (`FuncPtr` class)
 
-    // struct FunctionModel --> ReturnType & Arguments --> FuncPtr or Func
-
-    // Static callable type? (No heap alloc)
+    // struct CallableModel --> ReturnType & Arguments --> FuncPtr or Func
+    // Static callable type `Func`? (No heap alloc)
 }
