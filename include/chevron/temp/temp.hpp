0e53527feccf365ce32811a9caab28c80717aab4
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2026-01-18 17:48:05 -0500

    Removed Invalid Source 2/2
    Temporary stub entities now defined in explicit 'temp.hpp' and 'temp.cpp' files to prevent interference with development; Issue with build on Windows is suspected to be because of a lack of exported symbols which is likely causing the import library to not genereate (MSVC compiler), introduces explicit symbol to be exported

---
+++ include/chevron/temp/temp.hpp
@@ -0,0 +1,11 @@
+
+// NOTE: TEMPORARY HEADER
+
+#include "chevron/common/export.h"
+
+/*! @brief Stub class to allow generation of dll import lib. */
+class CHEVRON_API StubClass {
+public:
+    /*! @brief Pointless method. */
+    static void permitWin32ExportLib() noexcept;
+};
---
+++ src/temp/temp.cpp
@@ -0,0 +1,6 @@
+
+// NOTE: TEMPORARY STUB SOURCE
+
+#include "chevron/temp/temp.hpp"
+
+void StubClass::permitWin32ExportLib() noexcept { /* Does nothing */ }
