1100c8bbc4aaeeea3a180dd60bee7f5853d8eeba
Jamon Bailey <infinity.dev@outlook.com>
2026-03-29 04:05:15 -0400

    Update dev_exe.cpp

--- tests/cli/dev_exe.cpp
+++ tests/cli/dev_exe.cpp
@@ -4,27 +4,23 @@
 
 #include "chevron/function.hpp"
 
-int doNothing()
-{
-    return 15;
-}
-
 int main(int argc, char* argv[])
 {
     //\\//
     //
 
     // Start...
-    chevron::IntReturnNoArgs ggg{doNothing};
-    chevron::NoArgsReturn<int> ddd{doNothing};
 
     //
     //\\//
 
     return 0;
-
-    // FIX: Need specific constructor for lambda/functor using `::operator()`!!!
-    // (`Callable` class)
-    
-    // Static callable type `Func`? (No heap alloc)
 }
+
+
+/*
+ * :: Chevron Notes ::
+ * 
+ * [ Function Utilities Notes ]
+ * -> Static callable type `Func`? (No heap alloc)
+ */
