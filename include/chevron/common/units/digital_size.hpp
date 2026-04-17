984cf2c50ba9a9cbb6dc17e40711302ce607cc45
Jamon Bailey <83630529+jamon-bailey@users.noreply.github.com>
2026-04-17 17:08:14 -0400

    Digital Size Quantity Stubs
    Added skeleton for strongly-typed DigitalSize template as a stub; Still incomplete

---
+++ include/chevron/common/units/digital_size.hpp
@@ -0,0 +1,37 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and InfinSys, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3
+
+// [ISJTB-CXX-XL20260108-000003]
+
+/*!
+ * @file digital_size.hpp
+ *
+ * @brief
+ * Declaration of digital size quantity template.
+ *
+ * @author
+ * Jamon T. Bailey
+ *
+ * @date 04-17-2026
+ */
+
+#ifndef CHEVRON_LIB_DIGITAL_SIZE_TEMPLATE_H_
+#define CHEVRON_LIB_DIGITAL_SIZE_TEMPLATE_H_
+
+namespace chevron
+{
+
+/*!
+ * @brief
+ * Byte-based digital size quantity template.
+ *
+ * @details
+ * N/a
+ */
+template <typename ByteRatio>
+class DigitalUnits { /* TODO : INCOMPLETE IMPLEMENTATION!!! */ };
+
+}
+
+#endif // CHEVRON_LIB_DIGITAL_SIZE_TEMPLATE_H_
