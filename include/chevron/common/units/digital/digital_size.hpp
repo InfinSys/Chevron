7c1efb951e07940db777774627cdfea24dd5c980
Jamon Bailey <infinity.dev@outlook.com>
2026-04-19 03:56:57 -0400

    Added Arithmetic Operations to DigitalSize
    Introduce arithmetic and compound operators for DigitalSize; Moved class type traits above class definition

--- include/chevron/common/units/digital/digital_size.hpp
+++ include/chevron/common/units/digital/digital_size.hpp
@@ -28,6 +28,38 @@
 namespace chevron::units
 {
 
+// ===================================================================================== //
+//      <> chevron::units::DigitalSize | TYPE TRAITS
+// ===================================================================================== //
+
+namespace traits {
+
+/*!
+ * @brief
+ * Verify conversion between provided digital size units is lossless.
+ */
+template <typename From_Units, typename To_Units>
+constexpr bool is_lossless_size_conversion_v = std::ratio_divide<
+    typename From_Units::ByteRatio, typename To_Units::ByteRatio>::den == 1;
+
+/*!
+ * @brief
+ * Determine which of two digital size units is smaller.
+ */
+template <typename Units_A, typename Units_B>
+using finer_size_t = std::conditional_t<
+    std::ratio_less_v<typename Units_A::ByteRatio, typename Units_B::ByteRatio>, Units_A, Units_B>;
+
+/*!
+ * @brief
+ * Determine which of two digital size units is larger.
+ */
+template <typename Units_A, typename Units_B>
+using coarser_size_t = std::conditional_t<
+    !std::ratio_less_v<typename Units_A::ByteRatio, typename Units_B::ByteRatio>, Units_A, Units_B>;
+
+}
+
 /*!
  * @brief
  * TODO: INCOMPLETE DOCUMENTATION!!!
@@ -128,43 +160,129 @@ public:
     }
 
     // ===================================================================================== //
+    //      <> chevron::units::DigitalSize | OPERATORS
+    // ===================================================================================== //
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    template <uint64_t IncomingUnitBytes>
+    constexpr auto operator+(const DigitalSize<IncomingUnitBytes>& other) const noexcept
+    {
+        using FinerUnit = traits::finer_size_t<DigitalSize<UnitBytes>, DigitalSize<IncomingUnitBytes>>;
+
+        const ReprType totalBytes = bytes() + other.bytes();
+        return FinerUnit{static_cast<ReprType>(totalBytes / FinerUnit::ByteRatio::num)};
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    template <uint64_t IncomingUnitBytes>
+    constexpr auto operator-(const DigitalSize<IncomingUnitBytes>& other) const noexcept
+    {
+        using FinerUnit = traits::finer_size_t<DigitalSize<UnitBytes>, DigitalSize<IncomingUnitBytes>>;
+
+        const ReprType diffBytes = bytes() - other.bytes();
+        return FinerUnit{static_cast<ReprType>(diffBytes / FinerUnit::ByteRatio::num)};
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    constexpr DigitalSize<UnitBytes> operator*(const ReprType scalar) const noexcept
+    {
+        return DigitalSize<UnitBytes>{unitCount_ * scalar};
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    constexpr DigitalSize<UnitBytes> operator/(const ReprType scalar) const
+    {
+        if (scalar == 0) {
+            throw std::invalid_argument{"DigitalSize::operator/(): Division by zero."};
+        }
+
+        return DigitalSize<UnitBytes>{unitCount_ / scalar};
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    template <uint64_t IncomingUnitBytes>
+    constexpr double operator/(const DigitalSize<IncomingUnitBytes>& other) const
+    {
+        if (other.count() == 0) {
+            throw std::invalid_argument{ "DigitalSize::operator/(): Division by zero." };
+        }
+
+        return static_cast<double>(bytes()) / static_cast<double>(other.bytes());
+    }
+
+    // ===================================================================================== //
+    //      <> chevron::units::DigitalSize | COMPOUND OPERATORS
+    // ===================================================================================== //
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    template <uint64_t IncomingUnitBytes>
+    DigitalSize<UnitBytes>& operator+=(const DigitalSize<IncomingUnitBytes>& other) noexcept
+    {
+        const ReprType totalBytes = bytes() + other.bytes();
+        unitCount_ = static_cast<ReprType>(totalBytes / ByteRatio::num);
+        return *this;
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    template <uint64_t IncomingUnitBytes>
+    DigitalSize<UnitBytes>& operator-=(const DigitalSize<IncomingUnitBytes>& other) noexcept
+    {
+        const ReprType diffBytes = bytes() - other.bytes();
+        unitCount_ = static_cast<ReprType>(diffBytes / ByteRatio::num);
+        return *this;
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    DigitalSize<UnitBytes>& operator*=(const ReprType scalar) noexcept
+    {
+        unitCount_ *= scalar;
+        return *this;
+    }
+
+    /*!
+     * @brief
+     * TODO: INCOMPLETE DOCUMENTATION!!!
+     */
+    DigitalSize<UnitBytes>& operator/=(const ReprType scalar)
+    {
+        if (scalar == 0) {
+            throw std::invalid_argument{ "DigitalSize::operator/=(): Division by zero." };
+        }
+
+        unitCount_ /= scalar;
+        return *this;
+    }
+
+    // ===================================================================================== //
     //      <> chevron::units::DigitalSize | [PRIVATE] ATTRIBUTES
     // ===================================================================================== //
 private:
     ReprType unitCount_;
 };
-
-// ===================================================================================== //
-//      <> chevron::units::DigitalSize | TYPE TRAITS
-// ===================================================================================== //
-
-namespace traits {
-
-/*!
- * @brief
- * Verify conversion between provided digital size units is lossless.
- */
-template <typename From_Units, typename To_Units>
-constexpr bool is_lossless_size_conversion_v = std::ratio_divide<
-    typename From_Units::ByteRatio, typename To_Units::ByteRatio>::den == 1;
-
-/*!
- * @brief
- * Determine which of two digital units is smaller.
- */
-template <typename Units_A, typename Units_B>
-using finer_unit_t = std::conditional_t<
-    std::ratio_less_v<typename Units_A::ByteRatio, typename Units_B::ByteRatio>, Units_A, Units_B>;
-
-/*!
- * @brief
- * Determine which of two digital units is larger.
- */
-template <typename Units_A, typename Units_B>
-using coarser_unit_t = std::conditional_t<
-    !std::ratio_less_v<typename Units_A::ByteRatio, typename Units_B::ByteRatio>, Units_A, Units_B>;
-
-}
 
 }
 
