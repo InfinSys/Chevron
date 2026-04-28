2223d7504a72bf73d6f89356d1b502054b8369b9
Jamon Bailey <infinity.dev@outlook.com>
2026-04-28 05:26:06 -0400

    Memory Description Interface
    Introduced memory description interface for types that are centered around describing a region in memory

---
+++ include/chevron/memory/description.hpp
@@ -0,0 +1,138 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and InfinSys, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3
+
+// [ISJTB-CXX-XL20260108-000003]
+
+/*!
+ * @file description.hpp
+ *
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ *
+ * @author
+ * Jamon T. Bailey
+ *
+ * @date 04-28-2026
+ */
+
+#ifndef CHEVRON_LIB_MEMORY_DESCRIPTION_INTERFACE_H_
+#define CHEVRON_LIB_MEMORY_DESCRIPTION_INTERFACE_H_
+
+#include <cstddef>
+#include "chevron/common/units/digital/size_units.hpp"
+#include "chevron/common/export.h"
+
+namespace chevron::memory
+{
+
+/*!
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ * 
+ * @details
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ */
+class CHEVRON_API MemoryDescription {
+	// ===================================================================================== //
+    //      <> chevron::memory::MemoryDescription | CONSTRUCTORS / DESTRUCTOR
+    // ===================================================================================== //
+protected:
+	/*!
+	 * @brief
+	 * Construct memory description with base address, alignment
+	 * guarantee, and capacity.
+	 */
+	MemoryDescription(void* base, size_t alignment, units::Bytes size) noexcept;
+
+public:
+	virtual ~MemoryDescription() noexcept = default;
+
+	// ===================================================================================== //
+	//      <> chevron::memory::MemoryDescription | [PUBLIC] MEMBER METHODS
+	// ===================================================================================== //
+
+	/*!
+	 * @brief
+	 * Returns starting address of memory segment.
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] void* base() const noexcept;
+
+	/*!
+	 * @brief
+	 * Returns address alignment guarantee.
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] size_t alignment() const noexcept;
+
+	/*!
+	 * @brief
+	 * Returns size of memory segment in bytes.
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] units::Bytes size() const noexcept;
+
+	/*!
+	 * @brief
+	 * Returns byte count of memory segment.
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] size_t length() const noexcept;
+
+	/*!
+	 * @brief
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] std::byte* begin() const noexcept;
+
+	/*!
+	 * @brief
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] std::byte* end() const noexcept;
+
+	/*!
+	 * @brief
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] bool containsAddress(const void* ptr) const noexcept;
+
+	/*!
+	 * @brief
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 *
+	 * @return
+	 * TODO: INCOMPLETE DOCUMENTATION!!!
+	 */
+	[[nodiscard]] bool isAlignedTo(const size_t alignment) const noexcept;
+
+	// ===================================================================================== //
+	//      <> chevron::memory::MemoryDescription | [PRIVATE] ATTRIBUTES
+    // ===================================================================================== //
+private:
+	void* baseAddress_;
+	size_t alignment_;
+	size_t capacity_;
+};
+
+}
+
+#endif // CHEVRON_LIB_MEMORY_DESCRIPTION_INTERFACE_H_
---
+++ src/memory/description.cpp
@@ -0,0 +1,76 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and InfinSys, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3
+
+// [ISJTB-CXX-XL20260108-000003]
+
+/*!
+ * @file description.cpp
+ *
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ *
+ * @author
+ * Jamon T. Bailey
+ *
+ * @date 04-28-2026
+ */
+
+#include "chevron/memory/description.hpp"
+
+// ===================================================================================== //
+//      <> chevron::memory::MemoryDescription | CONSTRUCTORS / DESTRUCTOR
+// ===================================================================================== //
+
+chevron::memory::MemoryDescription::MemoryDescription(void* base, size_t alignment, units::Bytes size) noexcept
+	: baseAddress_{base}, alignment_{alignment}, capacity_{size.size_t_bytes()}
+{
+	//
+}
+
+// ===================================================================================== //
+//      <> chevron::memory::MemoryDescription | [PUBLIC] MEMBER METHODS
+// ===================================================================================== //
+
+void* chevron::memory::MemoryDescription::base() const noexcept
+{
+	return baseAddress_;
+}
+
+size_t chevron::memory::MemoryDescription::alignment() const noexcept
+{
+	return alignment_;
+}
+
+chevron::units::Bytes chevron::memory::MemoryDescription::size() const noexcept
+{
+	return units::Bytes{capacity_};
+}
+
+size_t chevron::memory::MemoryDescription::length() const noexcept
+{
+	return capacity_;
+}
+
+std::byte* chevron::memory::MemoryDescription::begin() const noexcept
+{
+	return static_cast<std::byte*>(baseAddress_);
+}
+
+std::byte* chevron::memory::MemoryDescription::end() const noexcept
+{
+	return static_cast<std::byte*>(baseAddress_) + capacity_;
+}
+
+bool chevron::memory::MemoryDescription::containsAddress(const void* ptr) const noexcept
+{
+	if (!ptr) return false;
+
+	const std::byte* givenAddress = static_cast<const std::byte*>(ptr);
+	return givenAddress >= this->begin() && givenAddress < this->end();
+}
+
+bool chevron::memory::MemoryDescription::isAlignedTo(const size_t alignment) const noexcept
+{
+	return (reinterpret_cast<uintptr_t>(baseAddress_) % alignment) == 0;
+}
