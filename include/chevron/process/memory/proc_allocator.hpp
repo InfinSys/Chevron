7e90ec0e288174e25066e8e39f6c3d94def18e9f
Jamon Bailey <infinity.dev@outlook.com>
2026-04-28 05:24:03 -0400

    Process Memory Allocator Class Stub

---
+++ include/chevron/process/memory/proc_allocator.hpp
@@ -0,0 +1,38 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and InfinSys, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3
+
+// [ISJTB-CXX-XL20260108-000003]
+
+/*!
+ * @file proc_allocator.hpp
+ *
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ *
+ * @author
+ * Jamon T. Bailey
+ *
+ * @date 04-28-2026
+ */
+
+#ifndef CHEVRON_LIB_PROCESS_MEMORY_ALLOCATOR_H_
+#define CHEVRON_LIB_PROCESS_MEMORY_ALLOCATOR_H_
+
+#include "chevron/memory/chunk_descriptor.hpp"
+
+namespace chevron::process
+{
+
+/*!
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ *
+ * @details
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ */
+class ProcessMemoryAllocator { /* TODO: INCOMPLETE IMPLEMENTATION!!! */ };
+
+}
+
+#endif // CHEVRON_LIB_PROCESS_MEMORY_ALLOCATOR_H_
---
+++ src/process/memory/proc_allocator.cpp
@@ -0,0 +1,21 @@
+
+// Copyright (C) 2026 by Jamon T. Bailey and InfinSys, LLC. All rights reserved.
+// Released under the terms of the GNU Affero General Public License version 3
+
+// [ISJTB-CXX-XL20260108-000003]
+
+/*!
+ * @file proc_allocator.cpp
+ *
+ * @brief
+ * TODO: INCOMPLETE DOCUMENTATION!!!
+ *
+ * @author
+ * Jamon T. Bailey
+ *
+ * @date 04-28-2026
+ */
+
+#include "chevron/process/memory/proc_allocator.hpp"
+
+// TODO : INCOMPLETE IMPLEMENTATION!!!
