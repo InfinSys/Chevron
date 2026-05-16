cabb1cd19112c26ea17bfa8b94026940bb9d4901
Jamon Bailey <infinity.dev@outlook.com>
2026-05-16 03:20:49 -0400

    Function Concepts Update
    Added general purpose void return signature verification concept

--- include/chevron/utility/function/concepts.hpp
+++ include/chevron/utility/function/concepts.hpp
@@ -15,7 +15,7 @@
 #define CHEVRON_LIB_HDR_FUNCTION_CONCEPTS_H_
 
 #include <concepts>
-#include <type_traits>
+#include "chevron/utility/function/type_traits.hpp"
 
 namespace chevron::func::concepts
 {
@@ -23,6 +23,12 @@ namespace chevron::func::concepts
 /*! @brief Concept that validates indexing into function parameters. */
 template <std::size_t Index, typename... ArgsT>
 concept valid_arguments_index = Index >= 0 && Index < sizeof...(ArgsT);
+
+/*! @brief Concept that validates a callable signature is void return. */
+template <typename CallableT>
+concept void_return_call = std::is_same_v<
+	typename traits::callable_signature<CallableT>::ReturnType, void
+>;
 
 } // namespace chevron::func::concepts
 
